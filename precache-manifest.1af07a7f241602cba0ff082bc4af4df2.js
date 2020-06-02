a60587720f4b20f6a888590ec024556a28ff017c
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2020-06-02 21:32:03 +0530

    Updates

---
+++ precache-manifest.1af07a7f241602cba0ff082bc4af4df2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9277191c2b97d967f828db640fec4ba7",
+    "url": "/github.io/qr-supplychain/index.html"
+  },
+  {
+    "revision": "2a988941d99fc8b67cbb",
+    "url": "/github.io/qr-supplychain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a21cfa4f7619d25d4423",
+    "url": "/github.io/qr-supplychain/static/css/main.5944b112.chunk.css"
+  },
+  {
+    "revision": "2a988941d99fc8b67cbb",
+    "url": "/github.io/qr-supplychain/static/js/2.10e13795.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/qr-supplychain/static/js/2.10e13795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21cfa4f7619d25d4423",
+    "url": "/github.io/qr-supplychain/static/js/main.e732fbcc.chunk.js"
+  },
+  {
+    "revision": "9ae9f4afc05f939db5ae",
+    "url": "/github.io/qr-supplychain/static/js/runtime-main.180937f3.js"
+  }
+]);
